0b7187feeff0d3af9824346ae2a9edde8b475633
Anton Makouski <85622715+AntonM030481@users.noreply.github.com>
2026-08-23 01:41:57 +0300

    Make point probe collapsible

--- src/ui/PointProbe.ts
+++ src/ui/PointProbe.ts
@@ -5,15 +5,17 @@ function fmt(value: number): string {
 }
 
 export class PointProbe {
-  readonly element: HTMLDivElement;
+  readonly element: HTMLDetailsElement;
   private readonly content: HTMLDivElement;
 
   constructor() {
-    this.element = document.createElement('div');
+    this.element = document.createElement('details');
     this.element.className = 'probe-panel';
-    this.element.innerHTML = '<b>POINT PROBE</b>';
+    this.element.open = true;
+    const summary = document.createElement('summary');
+    summary.innerHTML = '<b>POINT PROBE</b>';
     this.content = document.createElement('div');
-    this.element.appendChild(this.content);
+    this.element.append(summary, this.content);
     this.render(null);
   }
 
